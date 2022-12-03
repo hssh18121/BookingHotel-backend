e204b6ab46d0510f6c5a72e9feed1fce95704203
Sonle <leducson007@gmail.com>
2022-12-04 04:13:48 +0700

    remove old code

--- routes/hotelRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const hotelController = require("../controllers/hotelController");
-const router = express.Router();
-
-router.route("/hotels").get(hotelController.getAllHotels);
-router.route("/hotels/:id").get(hotelController.getHotel);
-router.route("/hotels/:id").delete(hotelController.deleteHotel);
-
-module.exports = router;
--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const userController = require("../controllers/userController");
-const router = express.Router();
-
-router.route("/users").get(userController.getAllUsers);
-router.route("/users/:id").get(userController.getUser);
-module.exports = router;
