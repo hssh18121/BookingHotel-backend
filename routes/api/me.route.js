20a9f6198c5e851ec785d075cd55a41249988634
Sonle <leducson007@gmail.com>
2022-12-28 19:36:02 +0700

    rebase

--- routes/api/me.route.js
+++ routes/api/me.route.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const meController = require("../../controllers/me.controller");
 const authMiddleware = require("../../middleware/auth.middleware");
-router.get("/:id", authMiddleware.protect, meController.getMe);
+router.get("/", authMiddleware.protect, meController.getMe);
 router.put("/password", authMiddleware.protect, meController.changePassword);
-router.put("/profile/:id", authMiddleware.protect, meController.updateProfile);
+router.put("/profile", authMiddleware.protect, meController.updateProfile);
 module.exports = router;
