e430ebd99f6b4a4fa8ac30d8be3beb53debb3ca6
Sonle <leducson007@gmail.com>
2023-01-03 00:47:14 +0700

    api: get rating(of Hotel)

--- routes/api/booking.route.js
+++ routes/api/booking.route.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const bookingController = require("../../controllers/booking.controller");
 const authMiddleware = require("../../middlewares/auth.middleware");
+router.get("/:roomId", authMiddleware.protect, bookingController.getAll);
 router.post("/:roomId", authMiddleware.protect, bookingController.booking);
 router.put("/:roomId", authMiddleware.protect, bookingController.update);
-router.delete("/:roomId", authMiddleware.protect, bookingController.delete);
+router.delete("/:bookingId", authMiddleware.protect, bookingController.delete);
 module.exports = router;
